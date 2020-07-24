1a4a03d9f1aa6c317e96706348ad7485306d51de
Mohamed Abdelrahman <eng.mohd.sherif@gmail.com>
2020-07-24 18:44:37 +0200

    Update web security configuration to allow swagger ui

--- src/main/java/com/codebase/security/WebSecurityConfiguration.java
+++ src/main/java/com/codebase/security/WebSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -36,6 +37,17 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.addFilterBefore(new JwtTokenFilter(userDetailsService), UsernamePasswordAuthenticationFilter.class);
     }
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// Allow swagger to be accessed without authentication
+		web.ignoring().antMatchers("/v2/api-docs")//
+				.antMatchers("/swagger-resources/**")//
+				.antMatchers("/swagger-ui.html")//
+				.antMatchers("/configuration/**")//
+				.antMatchers("/webjars/**")//
+				.antMatchers("/public");
+	}
 
     @Bean
     @Override
